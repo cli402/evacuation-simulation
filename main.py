0258e80963d7b21a3546eb70087798fa3e9bd6de
olen501@gmail.com <olen501@gmail.com@685ec306-030e-58a3-6d17-b0b9bf9d8acd>
2014-01-31 20:50:10 +0000

    updated main using the queue and threads

--- main.py
+++ main.py
@@ -4,15 +4,26 @@
  author: Daniel Henderson
 '''
 
+import generator
 import grid_info
 import ui_module
+import threading
+import Queue
 
 
 def main():
+    agent_queue = Queue.Queue()
     UI = ui_module.UserInterface(grid_info.grid_size, grid_info.tile_size, 'tech_square.jpg')
+    Gen = generator.Generator(agent_queue, grid_info.x_range, grid_info.y_range)
+
+    # start generator thread
+    gen_thread = threading.Thread(target=Gen.mainLoop)
+    gen_thread.daemon = True
+    gen_thread.start()
 
     while not UI.checkEvents():
-        UI.drawScreen(grid_info.update_grid())
+        coordinates = agent_queue.get()
+        UI.drawScreen(coordinates)
     UI.quit()
 
 
