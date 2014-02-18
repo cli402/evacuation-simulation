8cbcb24200da95542b71d87e6fba840da6dce69c
olen501@gmail.com <olen501@gmail.com@685ec306-030e-58a3-6d17-b0b9bf9d8acd>
2014-02-18 18:33:56 +0000

    updated main

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ from engine import Engine
 from terrain import Terrain
 from generator import Generator
 
+#random number generator
+import rand
+import time
+
+#for gaussian cdf
+from scipy.stats import norm
+
 
 def ui():
 	UI = ui_module.UserInterface(grid_info.grid_size, grid_info.tile_size, 'tech_square.jpg')
@@ -28,8 +35,19 @@ def ui():
 
 
 if __name__ == '__main__':
-	agent_queue = Queue.Queue()
-	engine = Engine(terrain = Terrain("map.dat"), generator_list = [Generator(5)])
-	print 'Starts to run simulation!'
-	while not engine.end_condition :
-		engine.step_simulate()
+    rand.srand(int(time.time()))
+    agent_queue = Queue.Queue()
+
+    gen_list = [
+        Generator(0, (grid_info.door_coords[0]['min'], grid_info.door_coords[0]['max']), grid_info.goal_coords, 5, rand.gaussian_rand, norm.cdf),
+        Generator(1, (grid_info.door_coords[1]['min'], grid_info.door_coords[1]['max']), grid_info.goal_coords, 7, rand.gaussian_rand, norm.cdf),
+        Generator(2, (grid_info.door_coords[2]['min'], grid_info.door_coords[2]['max']), grid_info.goal_coords, 10, rand.gaussian_rand, norm.cdf),
+        Generator(3, (grid_info.door_coords[3]['min'], grid_info.door_coords[3]['max']), grid_info.goal_coords, 8, rand.gaussian_rand, norm.cdf),
+        Generator(4, (grid_info.door_coords[4]['min'], grid_info.door_coords[4]['max']), grid_info.goal_coords, 13, rand.gaussian_rand, norm.cdf)
+    ]
+
+    engine = Engine(terrain = Terrain("map.dat"), generator_list = gen_list)
+
+    print 'Starts to run simulation!'
+    while not engine.end_condition :
+        engine.step_simulate()
